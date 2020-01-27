f85236e3c9d28da17f8f6162a061562f70a4ce1e
raphael-souza <raphael3130@gmail.com>
2020-01-26 23:39:22 -0300

    add novos heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -59,6 +59,14 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions)
+      .pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero - adicionar Hero'))
+      )
+  }
+
   private log(message: string) {
     this.messageService.add(`Heroservice: ${message}`)
   }
